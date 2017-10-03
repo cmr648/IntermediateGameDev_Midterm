c51f980e98300c605b9e01bb1242d5e4cf0ebc36
Unknown <cmr648@nyu.edu>
2017-10-02 21:56:19 -0400

    Initial Midterm Commit

---
+++ Assets/Scripts/Collect_Item.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collect_Item : MonoBehaviour {
+
+	public float Point_Increase; // the amount we will increase points by if an item is collected
+	public float Total_Points; // the total points the player currently has
+	public Text Point_Text; // creatinga  public variable for the text that will show points
+
+	// Use this for initialization
+	void Start () {
+		Total_Points = 0; // setting the total points to 0 at the start of a game
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Point_Text.text = "Points: " + Total_Points;
+		
+	}
+
+	void OnTriggerEnter(Collider col){ // checking to see if an object is collided with 
+
+		if (col.gameObject.tag == "Point") { // checking to see if the object collided with is tagged as point
+			Destroy (col.gameObject); // destroying the gameobject that is collected
+			Total_Points += Point_Increase;
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Driving_Controls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving_Controls : MonoBehaviour {
+
+	Vector3 Input_Vector; // creating a vector 3 that will function as all input commands
+	Rigidbody Player_Rigid_Body; // creating an rigidbody variable for our player
+	public float Player_Speed; // creating a public float to eddit the force our player moves at
+	public float Turn_Speed; // creating a variable for the speed of our turning
+	public float Player_Max_Speed; // creating a variable for the max speed we will allow the player to go
+
+	public GameObject Cam;
+
+	// Use this for initialization
+	void Start () {
+		Player_Rigid_Body = GetComponent<Rigidbody> (); // getting our players rigidbody and putting it in a variable
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float Input_Forward = Input.GetAxis ("Vertical"); // getting our vertical axis to move our player forwards
+		float Turning_Amount = Input.GetAxis ("Horizontal"); //getting our horizontal axis to rotate our player object
+
+		if(Player_Rigid_Body.velocity.magnitude < Player_Max_Speed){ // checking to see if our player velocity is goign to be less than the max speed we set
+		Player_Rigid_Body.AddForce (transform.forward * Player_Speed * Input_Forward); // adding force to our rigidbody in order to make our driving controls
+		}
+
+		gameObject.transform.Rotate(0f,Turning_Amount*Turn_Speed*Time.deltaTime,0f); // rotating our plyer game object based on the horizontal axis
+
+		Debug.Log (Vector3.Distance (transform.position, Cam.transform.position));
+	}
+
+	void FixedUpdate(){ // a void that will function every frame rather than every rendered frame
+		
+
+
+	}
+}
