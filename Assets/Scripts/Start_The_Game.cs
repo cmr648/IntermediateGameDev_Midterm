ba870fad705974e9a1078341b4a36cb500f48350
Unknown <cmr648@nyu.edu>
2017-10-12 09:54:49 -0400

    Added_Start_Game_Script

---
+++ Assets/Scripts/Start_The_Game.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Start_The_Game : MonoBehaviour {
+
+	public float Wait_Seconds; // creating a public float for when we should start the game
+	public Text Beginning_Text; // creating a public text element for our begining text
+	Driving_Controls Driving_Script; // creating a reference for our driving script
+	CountdownTimer Game_Timer;  // creating a reference for our gametimer
+
+	// Use this for initialization
+	void Start () {
+
+		Driving_Script = GetComponent<Driving_Controls>(); // assiging our driving controls
+		Game_Timer = GetComponent<CountdownTimer>(); // assiging our countdowntimer script
+		StartCoroutine("Wait_Then_Start"); // starting our coroutuine to begin the game
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Wait_Then_Start(){ // creating an IEneumerator for when we should wait to start the game
+		Driving_Script.enabled = false; // turning our driving controls on
+		Game_Timer.enabled = false; // turning off our game timer
+		Beginning_Text.text = "3"; // setting our beginning text
+		yield return new WaitForSeconds(Wait_Seconds); // asking our text to wait
+		Beginning_Text.text = "2"; // setting our beginning text
+		yield return new WaitForSeconds(Wait_Seconds); // asking our text to wait
+		Beginning_Text.text = "1"; // setting our beginning text
+		yield return new WaitForSeconds(Wait_Seconds); // asking our text to wait
+		Beginning_Text.text = "Start"; // setting our beginning text
+		Driving_Script.enabled = true; // setting our driving controls to be back on
+		Game_Timer.enabled = true; // setting our countdown timer to be back on
+		yield return new WaitForSeconds(Wait_Seconds); // asking our text to wait
+		Beginning_Text.enabled = false; // turning our begging text off
+	}
+}
